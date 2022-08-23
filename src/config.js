88a5a0d02d3d5f519ae3db5584b17e5972e50b73
IO-Chem <alton.kris@gmail.com>
2022-08-23 08:07:42 -0600

    config files for express server

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export const auth_endpoint = "https://accounts.spotify.com/authorize"
+export const client_id = "797eb26e2ddf469584ea5a4f01a3fa07"
+export const redirect_uri = "http://localhost:3000/auth/callback"
+export const scopes = [
+    "user-read-recently-played",
+    "playlist-read-collaborative",
+    "user-read-email",
+    "streaming",
+    "user-top-read",
+    "playlist-modify-public",
+    "user-follow-read",
+    "user-library-read",
+    "playlist-read-private",
+    "playlist-modify-private",
+    "user-read-private"
+]
