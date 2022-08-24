0fb23c64147bbf5fb51f5a5d30c1401d8c3bc4fd
IO-Chem <alton.kris@gmail.com>
2022-08-24 10:24:16 -0600

    add function to generate random string

--- server/src/serverConfig.js
+++ server/src/serverConfig.js
@@ -2,3 +2,12 @@ import { resolve } from "path";
 
 export const isProd = process.env.NODE_ENV === "production";
 export const distFolder = resolve(__dirname, "../dist");
+export function generateRandomString(length) {
+    let varChars = "";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < length; i++) {
+        varChars += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    return varChars
+};
