8ec6a1df3a542b697be38b34affbbcf87f8ec576
IO-Chem <alton.kris@gmail.com>
2022-08-24 12:32:36 -0600

    add login functionality to app

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   ChakraProvider,
   Box,
@@ -11,13 +11,30 @@ import {
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 
+import WebPlayback from './WebPlayback';
+import Login from './Login';
+
 function App() {
+
+  const [token, setToken] = useState('');
+  useEffect(() => {
+    async function getToken() {
+      const response = await fetch("/auth/token");
+      const json = await.response.json();
+      setToken(json.access_token);
+    };
+    getToken();
+  }, []);
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
+            <>
+              { (token === '') ? <Login/> : <WebPlayback token={token} /> }
+            </>
             <Text>
               Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
             </Text>
