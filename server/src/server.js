42d0418b393711546a76191986e7fc53a4c1c16b
IO-Chem <alton.kris@gmail.com>
2022-08-23 20:28:24 -0600

    change users router to auth

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
 
 const config = require("./serverConfig");
 
@@ -17,6 +17,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 module.exports = app;
